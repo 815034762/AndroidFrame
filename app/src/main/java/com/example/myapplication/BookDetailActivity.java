dc79474fb1592be82ad5cbf2f715b16fae9ada5f
zhangty <zhangty@szlanyou.com>
2020-10-10 11:29:56 +0800

    增加 BOOK详情页面

--- app/src/main/java/com/example/myapplication/BookDetailActivity.java
+++ app/src/main/java/com/example/myapplication/BookDetailActivity.java
@@ -5,13 +5,17 @@ import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModel;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.myapplication.demo.BaseData;
+import com.example.myapplication.glide.GlideImageLoader;
 import com.example.myapplication.viewmodel.model.BookDetail;
 
 import java.util.ArrayList;
@@ -22,6 +26,8 @@ import java.util.ArrayList;
  */
 public class BookDetailActivity extends AppCompatActivity {
 
+    private TextView mTvTitle,mTvDescription,mTvTime;
+    private ImageView mImageView;
     private BookDetailViewModel bookDetailViewModel;
     public static final String BOOK_ID = "bookId";
     private String bookId = "";
@@ -36,6 +42,12 @@ public class BookDetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_detail);
+
+        mTvTitle = findViewById(R.id.tv_title);
+        mTvDescription = findViewById(R.id.tv_desc);
+        mTvTime = findViewById(R.id.tv_time);
+        mImageView = findViewById(R.id.iv_img);
+
         bookId = getIntent().getStringExtra(BOOK_ID);
         bookDetailViewModel = ViewModelProviders.of(this).get(BookDetailViewModel.class);
         bookDetailViewModel.getBookDetail(bookId);
@@ -45,8 +57,12 @@ public class BookDetailActivity extends AppCompatActivity {
                 Log.e("tag", "===onChange===" + bookDetailBaseData.getData().getResult().getTotalNum());
                 if(bookDetailBaseData.getData() != null && !bookDetailBaseData.getData().getResult().getData().isEmpty()) {
                     ArrayList<BookDetail.ResultBean.DataBean> dataBeans = (ArrayList<BookDetail.ResultBean.DataBean>) bookDetailBaseData.getData().getResult().getData();
-                    for(BookDetail.ResultBean.DataBean dataBean : dataBeans) {
-
+                    if(!dataBeans.isEmpty()) {
+                        BookDetail.ResultBean.DataBean dataBean = dataBeans.get(0);
+                        mTvTime.setText(dataBean.getTitle());
+                        GlideImageLoader.get(BookDetailActivity.this).displayImage(dataBean.getImg(),mImageView);
+                        mTvTime.setText(dataBean.getBytime());
+                        mTvDescription.setText(dataBean.getSub1());
                     }
                 }
             }
---
+++ app/src/main/java/com/example/myapplication/glide/GlideImageLoader.java
@@ -0,0 +1,130 @@
+package com.example.myapplication.glide;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
+import com.bumptech.glide.request.RequestOptions;
+
+/**
+ * @author ly-zhangty
+ * 网络加载图片库
+ */
+public class GlideImageLoader {
+    public static GlideImageLoader mImageLoader;
+    private static Context mContext;
+
+    public static synchronized GlideImageLoader get(Context context) {
+        if (null == mImageLoader) {
+            mImageLoader = new GlideImageLoader();
+        }
+        mContext = context.getApplicationContext();
+        return mImageLoader;
+    }
+
+    /**
+     * 显示网络图片
+     *
+     * @param imageUrl  网络url
+     * @param imageView 需要加载图片的ImageView
+     */
+    public void displayImage(String imageUrl, ImageView imageView) {
+        RequestOptions requestOptions = RequestOptions.noTransformation().dontAnimate();
+        Glide.with(mContext).load(imageUrl).diskCacheStrategy(DiskCacheStrategy.ALL).apply(requestOptions).into(imageView);
+    }
+
+    /**
+     * 显示网络图片
+     *
+     * @param imageUrl     网络url
+     * @param imageView    需要加载图片的ImageView
+     * @param defaultImage 默认图片
+     */
+    public void displayImage(String imageUrl, ImageView imageView, int defaultImage) {
+        RequestOptions requestOptions = RequestOptions.noTransformation().placeholder(defaultImage).error(defaultImage).fallback(defaultImage).dontAnimate();
+        Glide.with(mContext).load(imageUrl).diskCacheStrategy(DiskCacheStrategy.ALL).apply(requestOptions).into(imageView);
+    }
+
+    /**
+     * 显示圆角网络图片
+     *
+     * @param imageUrl  网络url
+     * @param rounde    圆角半径
+     * @param imageView 需要加载图片的ImageView
+     */
+    public void displayRoundImageView(String imageUrl, int rounde, ImageView imageView) {
+        //设置图片圆角角度
+        RoundedCorners roundedCorners = new RoundedCorners(dp2px(mContext, rounde));
+        //通过RequestOptions扩展功能,override:采样率,因为ImageView就这么大,可以压缩图片,降低内存消耗
+        RequestOptions requestOptions = RequestOptions.bitmapTransform(roundedCorners).dontAnimate();
+        Glide.with(mContext).load(imageUrl).apply(requestOptions).diskCacheStrategy(DiskCacheStrategy.ALL).into(imageView);
+    }
+
+    /**
+     * 显示网络图片
+     *
+     * @param imageUrl     网络url
+     * @param rounde       圆角半径
+     * @param imageView    需要加载图片的ImageView
+     * @param defaultImage 默认图片
+     */
+    public void displayRoundImageView(String imageUrl, int rounde, ImageView imageView, @NonNull int defaultImage) {
+        //设置图片圆角角度
+        RoundedCorners roundedCorners = new RoundedCorners(dp2px(mContext, rounde));
+        //通过RequestOptions扩展功能,override:采样率,因为ImageView就这么大,可以压缩图片,降低内存消耗
+        RequestOptions requestOptions = RequestOptions.bitmapTransform(roundedCorners).placeholder(defaultImage).error(defaultImage).fallback(defaultImage).dontAnimate();
+        Glide.with(mContext).load(imageUrl).apply(requestOptions).diskCacheStrategy(DiskCacheStrategy.ALL).into(imageView);
+    }
+
+    /**
+     * 显示网络图片
+     *
+     * @param imageUrl  网络url
+     * @param imageView 需要加载图片的ImageView
+     */
+    public void displayImage(String imageUrl, ImageView imageView, int width, int height) {
+        RequestOptions requestOptions = RequestOptions.noAnimation().dontAnimate().override(width, height);
+        Glide.with(mContext).load(imageUrl).apply(requestOptions).diskCacheStrategy(DiskCacheStrategy.ALL).into(imageView);
+    }
+
+    /**
+     * 显示网络图片
+     *
+     * @param imageUrl     网络url
+     * @param imageView    需要加载图片的ImageView
+     * @param defaultImage 默认图片
+     */
+    public void displayImage(String imageUrl, ImageView imageView, @NonNull int defaultImage, int width, int height) {
+        RequestOptions requestOptions = RequestOptions.noAnimation().dontAnimate().placeholder(defaultImage).error(defaultImage).fallback(defaultImage).override(width, height);
+        Glide.with(mContext).load(imageUrl).apply(requestOptions).diskCacheStrategy(DiskCacheStrategy.ALL).into(imageView);
+    }
+
+    /**
+     * 显示网络图片
+     *
+     * @param imageUrl     网络url
+     * @param rounde       圆角半径
+     * @param imageView    需要加载图片的ImageView
+     * @param defaultImage 默认图片
+     */
+    public void displayRoundImageView(String imageUrl, int rounde, ImageView imageView, @NonNull int defaultImage, int width, int height) {
+        //设置图片圆角角度
+        RoundedCorners roundedCorners = new RoundedCorners(dp2px(mContext, rounde));
+        //通过RequestOptions扩展功能,override:采样率,因为ImageView就这么大,可以压缩图片,降低内存消耗
+        RequestOptions requestOptions = RequestOptions.bitmapTransform(roundedCorners).dontAnimate().placeholder(defaultImage).error(defaultImage).fallback(defaultImage).override(width, height);
+        Glide.with(mContext).load(imageUrl).apply(requestOptions).diskCacheStrategy(DiskCacheStrategy.ALL).into(imageView);
+    }
+
+    /**
+     * convert dp to its equivalent px
+     * 将dp转换为与之相等的px
+     */
+    public static int dp2px(Context context, float dipValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (dipValue * scale + 0.5f);
+    }
+}
