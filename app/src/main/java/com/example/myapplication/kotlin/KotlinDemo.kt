5c5e69655c5de93d3ecd75e05e6cf95b90c1c003
zhangty <zhangty@szlanyou.com>
2020-11-05 11:41:03 +0800

    提交kotlin代码

--- app/src/main/java/com/example/myapplication/kotlin/KotlinDemo.kt
+++ app/src/main/java/com/example/myapplication/kotlin/KotlinDemo.kt
@@ -3,10 +3,20 @@ package com.example.myapplication.kotlin
 class KotlinDemo {
 
     private var name: String? = null
-    private var age: Int? = null
-    val sex: String?
+    private var age: Int
+    get() {
+        return age
+    }
+    set(value) {
+        age = value
+    }
+
+    var sex: String
         get() {
-            TODO()
+            return sex
+        }
+        set(value) {
+            sex = value
         }
 
     private lateinit var kotlinDemo: KotlinDemo
@@ -24,11 +34,21 @@ class KotlinDemo {
         print(kotlinDemo?.toString())
     }
 
+    fun vars(vararg v:Int) {
+        for (vt in v) {
+            println(vt)
+        }
+    }
+
 }
 
 
 /** 我是main入口函数 **/
 fun main(args: Array<String>) {
-   var kotlinDemo = KotlinDemo()
+    var kotlinDemo = KotlinDemo()
     kotlinDemo.testNpe()
+    kotlinDemo.vars(12,232,43,43)
+    val a = 100;
+
+
 }
